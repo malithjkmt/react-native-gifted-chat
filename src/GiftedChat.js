7f934a6a6f6fb07d618437d907bde43d2ed05131
Malith <malithjkmt@users.noreply.github.com>
2018-07-13 11:54:32 +0530

    Facilitate react-native-action-button

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -476,9 +476,12 @@ class GiftedChat extends React.Component {
     if (this.state.isInitialized === true) {
       return (
         <ActionSheet ref={(component) => (this._actionSheetRef = component)}>
-          <View style={styles.container} onLayout={this.onMainViewLayout}>
-            {this.renderMessages()}
-            {this.renderInputToolbar()}
+          <View style={styles.container}>
+              <View style={styles.container} onLayout={this.onMainViewLayout}>
+                {this.renderMessages()}
+                {this.renderInputToolbar()}
+              </View>
+              {this.props.renderActionButton && this.props.renderActionButton()}
           </View>
         </ActionSheet>
       );
